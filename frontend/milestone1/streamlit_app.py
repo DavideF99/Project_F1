6b43421ca55b4f1a42787862c235d8de9a4f8d80
davide-ferreri <dferreri45@gmail.com>
2025-08-05 11:39:17 +0700

    Updated frontend with home page

--- frontend/milestone1/streamlit_app.py
+++ frontend/milestone1/streamlit_app.py
@@ -1,12 +1,13 @@
 import streamlit as st
 
 # Define the pages I want 
+home_page = st.Page("home_page.py", title="Home page")
 driver_page = st.Page("driver_page.py", title="Driver Info", icon="👤")
 team_page = st.Page("team_page.py", title="Team Info", icon="🏎️")
 race_page = st.Page("race_page.py", title="Race Info", icon="📍")
 
 # Set up navigation 
-pages = st.navigation([driver_page, team_page, race_page])
+pages = st.navigation([home_page, driver_page, team_page, race_page])
 
 # Run the selected page 
 pages.run()
